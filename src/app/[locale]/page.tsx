949ac753c30135df33aa57fd3241e0e23187bd1c
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2025-03-06 18:00:17 -0500

    Fix import paths for components in the locale page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,18 +3,18 @@ import { pick } from "lodash";
 import { useTranslations } from "next-intl";
 import { NextIntlClientProvider, useMessages } from "next-intl";
 
-import Head from "@/components/Head";
-import Navbar from "@/components/Navbar";
-import Wellcome from "@/components/Wellcome";
-import Services from "@/components/Services";
-import Team from "@/components/Team";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
-import Map from "@/components/Map";
-import PreFooter from "@/components/Prefooter";
-import Carousel from "@/components/Carousel";
-import About from "@/components/About";
-import MovingLine from "@/components/Moving-line";
+import Head from "../../components/Head";
+import Navbar from "../../components/Navbar";
+import Wellcome from "../../components/Wellcome";
+import Services from "../../components/Services";
+import Team from "../../components/Team";
+import Contact from "../../components/Contact";
+import Footer from "../../components/Footer";
+import Map from "../../components/Map";
+import PreFooter from "../../components/Prefooter";
+import Carousel from "../../components/Carousel";
+import About from "../../components/About";
+import MovingLine from "../../components/Moving-line";
 
 export default function Home() {
   const messages = useMessages();
